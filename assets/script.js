f19c03d4d730c30daf38712204056a4e49c04300
Krisko <chrishenri8@gmail.com>
2023-07-16 17:07:26 +0200

    initilisation arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,59 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//--------- variables ----------//
+
+// initialise for global scope //
+let shiftDirection = null;
+// copy slides for work on copy //
+let slidesToShow = Array.from(slides);
+// initialize slideNumber //
+let slideNumber = 1;
+
+
+//-------  Generate DOM elements  -------//
+const divBanner = document.querySelector("#banner");
+
+//-------  banner arrows  -------//
+const divArrow = document.createElement("div");
+
+divArrow.setAttribute("class", "arrow");
+
+//--------  left arrow  -------//
+const leftArrow = document.createElement("img");
+leftArrow.setAttribute("class", "arrow_left");
+leftArrow.src = "./assets/images/arrow_left.png";
+
+//--------  right arrow  -------//
+const rightArrow = document.createElement("img");
+rightArrow.setAttribute("class", "arrow_right");
+rightArrow.src = "./assets/images/arrow_right.png";
+
+//----------- banner img ------------//
+
+const bannerImg = document.createElement("img");
+
+bannerImg.src = `./assets/images/slideshow/${slidesToShow[0].image}`;
+
+bannerImg.setAttribute("class", "banner-img");
+bannerImg.setAttribute("alt", `slide-picture-${slideNumber}`);
+
+//--------  banner-tagline  ----------//
+
+const bannerTagLine = document.createElement("p");
+
+bannerTagLine.innerHTML = slidesToShow[0].tagLine;
+
+//---------  banner-dots  ------------//
+
+const divDots = document.createElement("div");
+divDots.setAttribute("class", "dots");
+
+divBanner.innerHTML = "";
+
+divBanner.appendChild(divArrow);
+divArrow.appendChild(leftArrow);
+divArrow.appendChild(rightArrow);
+divBanner.appendChild(bannerImg);
+divBanner.appendChild(bannerTagLine);
+divBanner.appendChild(divDots);
